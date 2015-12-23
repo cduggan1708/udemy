5b181b2c134f66081d91137953c8f87e0eaaac29
Caitlyn Duggan <cduggan1708@gmail.com>
2015-12-23 16:16:19 -0500

    testing

--- login.php
+++ login.php
@@ -59,8 +59,14 @@ if ((isset($_POST['username'])) && (isset($_POST['password']))) {
           // // $connection = new PDO("mysql:host=$db_host;dbname=$db;charset=utf8", $config_data['db_user'], $config_data['db_pwd']);
           // $connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
-          $connection = $app['pdo'];
-          $ps = $connection->prepare("SELECT username, password FROM udemy_automation.users where username=:username and password=:password");
+          $dbopts = parse_url(getenv('DATABASE_URL'));
+          $db_host = $dbopts['host'];
+          print_r($dbopts);
+          $db = ltrim($dbopts['path'],'/');
+          $connection = new PDO("pgsql:host=$db_host;port=5432;dbname=$db", $dbopts['user'], $dbopts['pass']);
+          $connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+          $ps = $connection->prepare("SELECT username, password FROM users where username=:username and password=:password");
           $ps->setFetchMode(PDO::FETCH_OBJ);
           $ps->bindParam(':username', $username);
           $ps->bindParam(':password', $password);
